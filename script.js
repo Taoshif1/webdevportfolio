d0a23cd3b5ceef7005441054f670935b381a0102
Taoshif1 <taoshif2@gmail.com>
2025-12-06 03:02:33 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+  const mobileToggle = document.getElementById('mobileToggle');
+  const navLinks = document.getElementById('navLinks');
+
+  mobileToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    mobileToggle.classList.toggle('toggle');
+  });
